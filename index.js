58cacea2d7e88e0c3b086a30a2348a3c27e7a403
Lucas Brun <lucbrun43000@gmail.com>
2025-12-07 19:38:54 +0100

    Création de l'index.js et de sa structure

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const bodyParser = require("body-parser");
+const hbs = require("hbs");
+
+const path = require("path");
+const app = express();
+const prisma = new PrismaClient();
+const PORT = 3008;
+
+
+
+// Configuration de Handlebars pour Express
+app.set("view engine", "hbs"); // On définit le moteur de template que Express va utiliser
+app.set("views", path.join(__dirname, "views")); // On définit le dossier des vues (dans lequel se trouvent les fichiers .hbs)
+hbs.registerPartials(path.join(__dirname, "views", "partials")); // On définit le dossier des partials (composants e.g. header, footer, menu...)
+
+// On définit un middleware pour parser les données des requêtes entrantes.
+// Cela permet de récupérer les données envoyées via des formulaires et les rendre disponibles dans req.body.
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Route pour la page d'accueil
+app.get("/", (req, res) => {
+    res.send("Bienvenue sur la page d'accueil de l'application Vapeur !");
+});
+
+//Gestion des erreurs 404 et 500
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Something broke!");
+});
+
+app.use((req, res, next) => {
+    res.status(404).send("Page not found");
+});
+
+// Démarrage du serveur
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
